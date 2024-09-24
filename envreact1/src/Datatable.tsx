1d46a22952650156193ebfae56249e99a2f7da49
Rashmi RanSingh <rashmi.ransingh@git.oasystspl.com>
2024-09-24 19:04:58 +0530

    3rd chg

---
+++ envreact1/src/Datatable.tsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+// Define the type for the data prop
+type Person = {
+  id: number;
+  name: string;
+  age: number;
+};
+
+type DataTableProps = {
+  data: Person[]; // Expect an array of Person objects
+};
+
+class DataTable extends Component<DataTableProps> {
+  render() {
+    const { data } = this.props;
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((person) => (
+            <tr key={person.id}>
+              <td>{person.id}</td>
+              <td>{person.name}</td>
+              <td>{person.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default DataTable;
